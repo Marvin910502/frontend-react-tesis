a541b4f35dbbe02aab57c7f6568464679aaafb7b
Marvin Urias Molto <m91urias@gmail.com>
2023-12-06 16:45:05 -0500

    permission fixed

--- src/components/navbarmini.tsx
+++ src/components/navbarmini.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import {Navbar, Container, Nav } from "react-bootstrap";
 import {Link, useLocation} from "react-router-dom";
 import Switch from "./switch";
 import SidebarDropdown from "./sidebar_dropdown";
 import { Home, SsidChart, QueryStats, DriveFolderUpload, Help } from "@mui/icons-material";
+import { UserContext } from "../context/context_provider";
 
 function NavbarMini(){
+
+    const user = useContext(UserContext)
 
     let theme: string | null = localStorage.getItem('themeMode')
 
@@ -30,11 +33,14 @@ function NavbarMini(){
         case '/mis-diagnosticos':
             list_event = '3'
             break
-        case '/subir-wrfout':
+        case '/maximos-minimos':
             list_event = '4'
-            break  
-        case '/ayuda':
+            break             
+        case '/subir-wrfout':
             list_event = '5'
+            break      
+        case '/ayuda':
+            list_event = '6'
             break        
     }
 
@@ -76,15 +82,25 @@ function NavbarMini(){
                                         Mis Diagnósticos
                                 </Nav.Link>
                             </Link>
-                            <Link className='text-decoration-none' to='subir-wrfout'>
-                                <Nav.Link href='subir-wrfout' eventKey='4' className={link_color}>
+                            <Link className='text-decoration-none' to='maximos-minimos'>
+                                <Nav.Link href='maximos-minimos' eventKey='4' className={link_color}>
+                                    <SsidChart className='me-2' />
+                                        Máximos y Mínimos
+                                </Nav.Link>
+                            </Link>
+                            {   user.user.isManager ?
+                                <Link className='text-decoration-none' to='subir-wrfout'>
+                                <Nav.Link href='subir-wrfout' eventKey='5' className={link_color}>
                                     <DriveFolderUpload className='me-2' />
                                         Subir WRFout 
                                 </Nav.Link>
-                            </Link>
+                                </Link>
+                                :
+                                <></>
+                            }
                             <hr/>
                             <Link className='text-decoration-none' to='ayuda'>
-                                <Nav.Link href='ayuda' eventKey='5' className={link_color}>
+                                <Nav.Link href='ayuda' eventKey='6' className={link_color}>
                                     <Help className='me-2' />
                                         Ayuda 
                                 </Nav.Link>
